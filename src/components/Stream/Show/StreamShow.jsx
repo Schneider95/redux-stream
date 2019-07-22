fd7ff21faef86844fa0e5c75a36c0a6cb63c5d97
ASI <arnaud.sartori@popsiit.net>
2019-07-22 17:48:40 +0200

    temp

--- src/components/Stream/Show/StreamShow.jsx
+++ src/components/Stream/Show/StreamShow.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
-const StreamShowJsx = props => (
-  <div>StreamShow</div>
-);
+class StreamShowJsx extends React.Component {
+
+  constructor(props) {
+    super(props);
+    const { stream } = props;
+
+    this.props.stream = stream;
+  }
+
+  render = () => {
+    const { stream } = this.props;
+
+    return (
+      <div>
+        {typeof this.props.stream !== 'undefined' &&
+          <div>
+            <h1>{this.props.stream.title}</h1>
+            <h5>{this.props.stream.description}</h5>
+          </div>
+        }
+
+        {typeof this.props.stream === 'undefined' &&
+          <div>Loading.....</div>
+        }
+
+      </div>
+    );
+  }
+}
 
 export default StreamShowJsx;
--- src/components/Stream/Show/index.js
+++ src/components/Stream/Show/index.js
@@ -1,5 +1,58 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
 import StreamShowJsx from './StreamShow';
+import { fetchStreamAction } from '../../../actions';
 
-const StreamShow = () => StreamShowJsx();
+class StreamShow extends React.Component {
+  componentDidMount = () => {
+    const { fetchStream, match } = this.props;
 
-export default StreamShow;
+    fetchStream(match.params.id);
+  };
+
+  render = () => {
+    const { stream } = this.props;
+
+    const props = {
+      stream,
+    };
+
+    return new StreamShowJsx(props).render();
+  };
+}
+
+StreamShow.defaultProps = {
+  stream: undefined,
+};
+
+StreamShow.propTypes = {
+  fetchStream: PropTypes.func.isRequired,
+  match: PropTypes.objectOf(
+    PropTypes.oneOfType([
+      PropTypes.func,
+      PropTypes.bool,
+      PropTypes.number,
+      PropTypes.string,
+      PropTypes.objectOf(PropTypes.string),
+    ]),
+  ).isRequired,
+  stream: PropTypes.objectOf(
+    PropTypes.oneOfType([
+      PropTypes.func,
+      PropTypes.bool,
+      PropTypes.number,
+      PropTypes.string,
+    ]),
+  ),
+};
+const mapStateToProps = (state, ownProps) => ({ stream: state.streams[ownProps.match.params.id] });
+
+const mapDispatchToProps = {
+  fetchStream: fetchStreamAction,
+};
+
+const StreamShowConnect = connect(mapStateToProps, mapDispatchToProps);
+const StreamShowConnected = StreamShowConnect(StreamShow);
+
+export default StreamShowConnected;
