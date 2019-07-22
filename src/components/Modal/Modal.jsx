71ea1933a453a1b4ca287259b2040bc3229bc459
ASI <arnaud.sartori@popsiit.net>
2019-07-22 16:50:48 +0200

    fix modal jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,30 +1,25 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 
-class ModalJsx extends React.Component {
-  render = () => {
-    return (
+const ModalJsx = (props) => {
+  return (
+    <div
+      className="ui dimmer modals visible active"
+      onClick={props.onDismiss}
+      role="menuitem"
+      tabIndex="0"
+    >
       <div
-        className="ui dimmer modals visible active"
-        onClick={this.onDismiss}
-        onKeyPress={this.handleKeyPress}
-        role="menuitem"
-        tabIndex="0"
+        className="ui standard modal visible active"
+        onClick={e => e.stopPropagation()}
+        role="tab"
+        tabIndex="-1"
       >
-        <div
-          className="ui standard modal visible active"
-          onClick={e => e.stopPropagation()}
-          onKeyPress={this.handleKeyPress}
-          role="tab"
-          tabIndex="-1"
-        >
-          <div className="header">{this.props.title}</div>
-          <div className="content">{this.props.content}</div>
-          <div className="actions">{this.props.actions}</div>
-        </div>
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default ModalJsx;
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,29 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import ModalJsx from './Modal';
 
-const Modal = (props) => {
-  return ReactDOM.createPortal(
-    <div
-      className="ui dimmer modals visible active"
-      onClick={props.onDismiss}
-      role="menuitem"
-      tabIndex="0"
-    >
-      <div
-        className="ui standard modal visible active"
-        onClick={e => e.stopPropagation()}
-        role="tab"
-        tabIndex="-1"
-      >
-        <div className="header">{props.title}</div>
-        <div className="content">{props.content}</div>
-        <div className="actions">{props.actions}</div>
-      </div>
-    </div>
-    ,
-    document.querySelector('#modal')
-  );
-};
+const Modal = props => ReactDOM.createPortal(
+  ModalJsx(props),
+  document.querySelector('#modal'),
+);
 
 export default Modal;
